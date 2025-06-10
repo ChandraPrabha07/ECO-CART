f0beb3033df4d4b9cba6329f5e00bde2b937acac
ChandraPrabha07 <2403717674422010@cit.edu.in>
2025-06-10 10:39:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
-  
 };
 
-module.exports = nextConfig;
+export default nextConfig;
